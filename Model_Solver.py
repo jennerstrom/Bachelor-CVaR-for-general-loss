819d384e91f0f36366cf94719686346e10676a3f
Jakob Juul Hansen <141743992+jakob-the-killer@users.noreply.github.com>
2024-03-17 12:08:39 +0100

    Update Model_Solver.py

--- Model_Solver.py
+++ Model_Solver.py
@@ -4,10 +4,11 @@ import numpy as np
 import random
 import math
 import pandas as pd
+import matplotlib.pyplot as plt
 
 params = {
 "WLSACCESSID": '730eb8a4-9f5d-46f4-a00a-5a5a124b5cfc',
-"WLSSECRET": 'fb659f16-0ba2-4a5d-8740-c85a5ba53c04',
+"WLSSECRET": 'INSERT SECRET PLEASE',
 "LICENSEID": 2482509,
 }
 env = gp.Env(params=params)
@@ -17,6 +18,7 @@ env = gp.Env(params=params)
 ### DATA GENERATION
 ###
 
+# We define functions that generate the data for the model, the logic of these are taken from the Y. Hinosha article
 
 def unit_shipment_cost(I,J,L,p):
     d = []
@@ -72,7 +74,6 @@ def split_int(n):
     c = math.ceil((n-a-b)/2)
     d = math.floor((n-a-b)/2)
     return [d,c,b,a]
-
 
 def demander(J,S,L):
     scenarios = split_int(S)
@@ -208,11 +209,6 @@ def optimal_cvar (shipment_cost,handling_cost,setup_cost,demand,supply_cap,alpha
         for l in range(L):
             model.addConstr(gp.quicksum(supply_cap[s,l,i]*z[s, i, l] for i in range(I+1)) >= gp.quicksum(demand[s,j,l] for j in range(J)))
 
-    # Overvej lige denne constraint
-    #for s in range(S):
-    #  for i in range(I):
-    #    model.addConstrs(gp.quicksum(y[i,j] for j in range(J)) >= z[i,s])
-
     # solving problem
     model.Params.outputFlag = 0
     model.Params.LogToConsole = 0
@@ -220,25 +216,89 @@ def optimal_cvar (shipment_cost,handling_cost,setup_cost,demand,supply_cap,alpha
     return [model, y.X, x.X, z.X]
 
 # Set parameters
-#I = 2
-#J = 3
-#S = 2
-#L = 2
-#alpha = 0.95
-#
-## Solve the model
+I = 2
+J = 3
+S = 25
+L = 2
+alpha = 0.95
+
+# Here we can solve the model for a single set of parameters to verify that the model works i.e. the solution seems
+# reasonable and the model is feasible
+
+## Generate data
 #unit_penalty = unitary_penalty(I,J,L)
 #shipment_cost = unit_shipment_cost(I,J,L,unit_penalty)
 #handling_cost = fixed_handling_cost(I,L)
 #setup_cost  = set_up_cost(I,J,L)
 #demand = demander(J,S,L)
 #supply_cap = max_supply(I,J,L,demand)
-#
+
+## Solve model
 #solution = optimal_cvar(shipment_cost,handling_cost,setup_cost,demand,supply_cap,alpha,S,I,J,L)
 #model = solution[0]
 #print(f'Objective value: {model.ObjVal}')
 #print(f'Activated links: {solution[1]}')
 #print(f'Flows: {solution[2]}')
+
+## In order to verify that the model works we can generate a plot that shows the relationship between actual cost and
+## CVaR. (see PDF)
+
+# Set parameters
+I = 2
+J = 3
+S = 25
+L = 2
+alpha = 0.95
+points = 100
+
+def actual_cost(S, setup_cost, shipment_cost, handling_cost, y, x, z):
+    scenario = np.random.randint(0,S)
+    cost = (gp.quicksum(setup_cost[j, i]*y[i, j] for i in range(I+1) for j in range(J))+
+                     gp.quicksum(shipment_cost[l, j, i]*x[scenario,l, i, j] for l in range (L) for i in range(I+1) for j in range(J))+
+                     gp.quicksum(handling_cost[l,i]*z[scenario, i, l] for l in range(L) for i in range(I+1)))
+    return cost
+
+def generate_plot(points, I, J, S, L, alpha):
+    list = []
+    for i in range(points):
+        # Generate data
+        unit_penalty = unitary_penalty(I, J, L)
+
+        shipment_cost = unit_shipment_cost(I, J, L, unit_penalty)
+
+        handling_cost = fixed_handling_cost(I, L)
+
+        setup_cost = set_up_cost(I, J, L)
+
+        demand = demander(J, S, L)
+
+        supply_cap = max_supply(I, J, L, S, demand)
+
+        # optimize model
+        solution = optimal_cvar(shipment_cost, handling_cost, setup_cost, demand, supply_cap, alpha, S, I, J, L)
+        if solution[0].status != GRB.OPTIMAL:
+            print("Model not optimal")
+            break
+        list.append((actual_cost(S, setup_cost, shipment_cost, handling_cost, solution[1], solution[2], solution[3]), solution[0].objVal))
+
+    #plot the data from the list where the first element is the actual cost and the second element is the objective value
+    y = [i[0].getValue() for i in list]
+    x = [i[1] for i in list]
+
+    x_line = np.arange(min(x), max(x), 0.1)
+    y_line = x_line
+
+    plt.scatter(x, y, label = "Actual cost")
+    plt.plot(x_line, y_line, label = "y = x")
+    plt.xlabel('Objective value')
+    plt.ylabel('Actual cost')
+    plt.title(f'Actual cost vs Objective value, alpha = {alpha}, I = {I}, J = {J} S = {S}')
+    return plt.show()
+
+generate_plot(points, I, J, S, L, alpha)
+
+## We now turn our attention to the report where we will generate data, and solve the model for different values of our
+## parameters and present the results in a table
 
 def data_generator (origins, destinations, scenarios, commodities):
     df = pd.DataFrame(
@@ -291,64 +351,12 @@ scenarios = [3,4,5]
 commodities = [1,2]
 alpha = 0.95
 
+# Here we can generate the table that we present in our report, where we can select the parameters we want to use
+
 #data = data_generator(origins, destinations, scenarios, commodities)
 #table = generate_table(data, origins, destinations, scenarios, commodities, alpha)
-
 #print(table)
 
-###
-### Figures
-###
+# We have made another script that can turn this table into a latex table, so we can easily include it in our report
 
-# Set parameters
-I = 2
-J = 3
-S = 25
-L = 2
-alpha = 0.95
-
-def actual_cost(S, setup_cost, shipment_cost, handling_cost, y, x, z):
-    scenario = np.random.randint(0,S)
-    cost = (gp.quicksum(setup_cost[j, i]*y[i, j] for i in range(I+1) for j in range(J))+
-                     gp.quicksum(shipment_cost[l, j, i]*x[scenario,l, i, j] for l in range (L) for i in range(I+1) for j in range(J))+
-                     gp.quicksum(handling_cost[l,i]*z[scenario, i, l] for l in range(L) for i in range(I+1)))
-    return cost
-
-list = []
-for i in range(50):
-    # Generate data
-    unit_penalty = unitary_penalty(I, J, L)
-
-    shipment_cost = unit_shipment_cost(I, J, L, unit_penalty)
-
-    handling_cost = fixed_handling_cost(I, L)
-
-    setup_cost = set_up_cost(I, J, L)
-
-    demand = demander(J, S, L)
-
-    supply_cap = max_supply(I, J, L, S, demand)
-
-    # optimize model
-    solution = optimal_cvar(shipment_cost, handling_cost, setup_cost, demand, supply_cap, alpha, S, I, J, L)
-    if solution[0].status != GRB.OPTIMAL:
-        print("Model not optimal")
-        break
-    list.append((actual_cost(S, setup_cost, shipment_cost, handling_cost, solution[1], solution[2], solution[3]), solution[0].objVal))
-
-
-#plot the data from the list where the first element is the actual cost and the second element is the objective value
-import matplotlib.pyplot as plt
-y = [i[0].getValue() for i in list]
-x = [i[1] for i in list]
-
-x_line = np.arange(min(x), max(x), 0.1)
-y_line = x_line
-
-plt.scatter(x, y, label = "Actual cost")
-plt.plot(x_line, y_line, label = "y = x")
-plt.xlabel('Objective value')
-plt.ylabel('Actual cost')
-plt.title(f'Actual cost vs Objective value, alpha = {alpha}, I = {I}, J = {J} S = {S}')
-plt.show()
 
